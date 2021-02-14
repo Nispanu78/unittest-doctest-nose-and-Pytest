1fb7705301ef0eaa2ebe32968557ab3aa59b350b
Nicola <nicspanu@gmail.com>
2021-02-14 13:09:50 +0100

    nose 6

--- nose/test/__init__.py
+++ nose/test/__init__.py
@@ -1,1 +1,7 @@
-all = ["test_module01", "test_module02"]
+def setUpPackage():
+    print("In setUpPackage()...")
+
+    all = ["test_module01", "test_module02", "test_module03", "test_module04"]
+
+def tearDownPackage():
+    print("In tearDownPackage()...")
